babc68ae6b6b830377abd7063ff3653bea62f39b
main1 <main1@main1s-MacBook-Air.local>
2020-04-05 22:08:16 -0700

    added in single test, eandata database working

--- src/BarcodeCompany.py
+++ src/BarcodeCompany.py
@@ -2,31 +2,29 @@ import requests
 import config
 
 
-
 # Identify company name from barcode via API call.
 class BarcodeCompany:
     def __init__(self, barcode):
-        self.name = barcode
+        self.barcode = barcode
     
-    def _get_barcode_company_data (self):
-        # TODO: Make Barcode to Company API call.
-        self._process_upc
-        return
-        
     # Should this be recursive instead?
-    # TODO: Figure out what to return and if to send any error messages.   
-    def _process_upc (self):
-      r = requests.get(config.EANDATA_BARCODE_API_ENDPOINT + self.barcode)
-      if r.status_code == 200:
-        data = r.json()
-        if len(data["items"]) > 0:
-          print(data["items"][0]["brand"])
-          return
-      r = requests.get(config.UPCITEMDB_BARCODE_API_ENDPOINT + self.barcode)
-      if r.status_code == 200:
-          data = r.json()
-          if len(data["items"]) > 0:
-            print(data["items"][0]["brand"])
-            return
-      print("No matches for barcode")
-      return
+    # TODO: Figure out what to return and if to send any error messages.  
+    # TODO: Add logging. 
+    def get_barcode_company_data (self):
+      r = requests.get(config.EANDATA_BARCODE_API_ENDPOINT + self.barcode).json()
+      company = r.get("company")
+      if company:
+        return company.get("name")
+      # TODO: Check another database for barcode.
+      # company_name is set to None when response lacks company name.
+      return None
+
+
+# Tests if barcode to company.
+# Uses Thayer Witch Hazel.
+thayer_witch_hazel_barcode = "041507070066"
+barcode_test = BarcodeCompany(thayer_witch_hazel_barcode)
+if barcode_test.get_barcode_company_data() == "Thayer":
+  print("Barcode to company test passing!")
+else:
+  print("Barcode to company test failing!")
--- src/config.py
+++ src/config.py
@@ -2,7 +2,5 @@ import os
 
 OS_API_KEY = os.getenv('OSKEY')
 EANDATA_BARCODE_API_KEY = os.getenv('EANDATA_KEY')
-EANDATA_BARCODE_API_ENDPOINT = "https://eandata.com/feed/?v=3&keycode={EANDATA_BARCODE_API_KEY}&mode=json&find="
+EANDATA_BARCODE_API_ENDPOINT = f"https://eandata.com/feed/?v=3&keycode={EANDATA_BARCODE_API_KEY}&mode=json&find="
 UPCITEMDB_BARCODE_API_ENDPOINT = "https://api.upcitemdb.com/prod/trial/lookup?upc="
-
-
